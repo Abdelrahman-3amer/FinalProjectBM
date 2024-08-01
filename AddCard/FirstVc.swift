b7284072d4bf9dd51fc46bec38839d57be428adf
roqaya141 <roqayahassan40@gmail.com>
2024-08-01 19:42:41 +0300

    add print statement

---
+++ AddCard/FirstVc.swift
@@ -0,0 +1,20 @@
+//
+//  FirstVc.swift
+//  FinalProjectBM
+//
+//  Created by SONOMA on 01/08/2024.
+//
+
+import UIKit
+
+class FirstVc: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
